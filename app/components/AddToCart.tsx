dac35f74ffd8493d94cd4b5f518072b18a8ed408
Romeo Enso <romeo.enso@connexionltd.com>
2023-10-11 09:40:54 +1100

    fundamentals -  routing, data fetching

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button onClick={() => console.log("add to cart")}>Add To Cart</button>
+  );
+};
+
+export default AddToCart;
