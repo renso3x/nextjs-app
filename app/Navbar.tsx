8380043abb18c3dcabaf93b21a65ad305c1fdb57
Romeo Enso <romeo.enso@connexionltd.com>
2023-10-12 09:42:06 +1100

    layout pages

---
+++ app/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className="flex bg-slate-200 p-5">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users" className="mr-5">
+       Users
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AdminHomePage = () => {
+  return (
+    <div>
+        Admin Home Page
+
+    </div>
+  )
+}
+
+export default AdminHomePage
