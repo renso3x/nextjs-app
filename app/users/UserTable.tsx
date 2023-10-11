06e7ac6370eaaddd1879d122261f28d5d211fd24
Romeo Enso <romeo.enso@connexionltd.com>
2023-10-12 09:34:30 +1100

    routing - accessing query string, params

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import { sort } from 'fast-sort'
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface Props {
+    sortOrder: string
+}
+
+const UserTable = async({ sortOrder }: Props) => {
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  const sortedUsers = sort(users).asc(sortOrder === 'email' ? user => user.email : user => user.name)
+
+  return (
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>
+              <Link href={"/users?sortOrder=name"}>Name</Link>
+            </th>
+            <Link href={"/users?sortOrder=email"}>Email</Link>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedUsers.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserTable;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,21 @@
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
+interface Props {
+  searchParams: {
+    sortOrder: string
+  }
 }
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
+
+
+const UsersPage = async ({
+  searchParams: {
+    sortOrder
+  }
+}: Props) => {
 
   return (
-    <div>
-      UsersPage
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <UserTable sortOrder={sortOrder}/>
   );
 };
 
