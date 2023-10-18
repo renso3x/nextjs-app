ae67d08198704b9dc371a4411a64153ab8c4901c
Romeo Enso <romeo.enso@connexionltd.com>
2023-10-19 09:50:05 +1100

    image optimzation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
 
 export default async function Home() {
-
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
-    <main>
+    <main className="relative h-screen">
       <h1>Helo {session && <span>{session.user!.name}</span>}</h1>
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="my-profile"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw,33vw"
+        priority
+      />
     </main>
   );
 }
